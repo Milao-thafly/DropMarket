3a7537728dc1035cd5ff1bfd6e6b9c79b391d50f
Martin Boizard <martinboizard@yahoo.fr>
2025-10-09 14:41:54 +0200

    Initialisation fichier de base

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,1 +1,29 @@
-import Express from "express"
+import Express, { Router } from "express";
+import { fileURLToPath } from "node:url";
+import path from "node:path"
+import  router from "./route/index";
+
+
+const app = Express();
+const PORT = 3000;
+
+// @ts-ignore
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+app.use(Express.urlencoded({ extended: true}));
+
+
+app.use("/", router);
+
+app.use(Express.static(path.join(__dirname, "public")));
+
+
+app.use((req,res) => {
+    res.status(404).send("Page non trouvée")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+});
