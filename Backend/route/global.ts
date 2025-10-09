4d8aade07eef77338af164f2b6aa087a107894b9
indidi <indian.v01@gmail.com>
2025-10-09 15:01:52 +0200

    mise en place du globalrouter

--- Backend/route/global.ts
+++ Backend/route/global.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { GlobalController } from "../controllers/GlobalController";
 
-const GlobalRouter = Router();
 
-GlobalRouter.get("/", (request, response) => {
+const globalRouter = Router();
+
+globalRouter.get("/", (request, response) => {
     const controller = new GlobalController(request, response);
     controller.homepage()
 })
 
-export default globalRouter
+export default globalRouter;
--- Backend/route/index.ts
+++ Backend/route/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-
+import globalRouter from './global';
 
 const router = Router();
 
-
-
-
+router.use(globalRouter);
 
 export default router;
