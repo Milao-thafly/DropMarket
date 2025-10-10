c649f5ed74a5a91a3fa8d0b89a8a833e5877f696
Martin Boizard <martinboizard@yahoo.fr>
2025-10-10 14:21:33 +0200

    ProductController with productRouter

---
+++ Backend/controllers/ProductController.ts
@@ -0,0 +1,14 @@
+import { Controller } from "../libs/Controller"
+
+export class CreateProductController extends Controller {
+    public createProductRender() {
+        this.response.json(())
+    }
+}
+
+
+export class BrowseProductController extends Controller {
+    public browseProductRender() {
+        this.response.json(())
+    }
+}
