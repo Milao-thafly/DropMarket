a74d37aabb5822e119a65d979231f3e13bf6f4d9
azriel750 <marc.ubertalli.simplon@gmail.com>
2025-10-14 16:42:44 +0200

    routerviews mise a jour de database

--- Backend/index.ts
+++ Backend/index.ts
@@ -29,5 +29,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}`);
+  console.log(` Server is running on http://localhost:${PORT}`);
 });
