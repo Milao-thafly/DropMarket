efae8deb08b98b98fd9d1007650d68a217109c4d
indidi <indian.v01@gmail.com>
2025-10-15 16:32:13 +0200

    back + front homepage opérationnel

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserPage from "../views/Users";
 import "./App.css";
+import HomePage from "../views/HomePage";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route />
+          <Route path="/" element={<HomePage />} />
           <Route path="/user" element={<UserPage />} />
         </Routes>
       </Router>
