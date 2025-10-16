c5287e9d04bd2265bb19121f597fcc2caa3521f8
azriel750 <marc.ubertalli.simplon@gmail.com>
2025-10-16 14:17:59 +0200

    mise en place du contexte et authentification de users

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserPage from "../views/Users";
-import "./App.css";
-import HomePage from "../views/HomePage";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { AuthProvider } from "./context/Authcontext"
+import reactLogo from './assets/react.svg'
+import UserPage from "../views/Users"
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 
 function App() {
   return (
